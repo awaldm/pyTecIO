1fc1c021e2f144ce4f0a55c77504f3cf369db847
awaldm <mail@awaldmann.com>
2022-11-11 14:40:39 +0100

    made package installable by adding setup.py, updated reader. ASCII reader is now called read_ascii() instead of readnew()

--- pytecio/read_tecplot.py
+++ pytecio/read_tecplot.py
@@ -31,8 +31,15 @@ def is_number(s):
     return False
 
 
-def read_tecplot(filename, verbose):
-
+'''
+readnew is kind of the definitive version of the tecplot reader: it reads the Tecplot file and returns all the zones and variables
+TODO: currently this can be fooled by lowercase Tecplot keywords (i.e. "zone T=" instead of "ZONE T=")
+TODO: untested with structured 2D zones - though this should work. still, parse the I= J= etc data, this should be possible
+TODO: needs more testing for unstructured zones
+TODO: what about AUXDATAi?
+TODO: returned data is type object, needs to be cast to float before plotting etc. why is to_numeric not taking care of this?
+'''
+def read_ascii(filename, verbose):
     datafile = open(filename, "r")
 
     struct=1
@@ -50,37 +57,48 @@ def read_tecplot(filename, verbose):
     els = []
     for line in datafile:
         comm_match = line.find("#")
-        title_match = line.find("TITLE")
-        var_match = line.find("VARIABLES")
-        zone_match = line.find("ZONE T")
-        dt_match = line.find("DT=(")
+        title_match = line.upper().find("TITLE")
+        var_match = line.upper().find("VARIABLES")
+        zone_match = line.upper().find("ZONE T")
+        dt_match = line.upper().find("DT=(")
         nodes_match = line.find("Nodes=")
         elems_match = line.find("Elements=")
+
+        #print(is_number(line.split()[0]))
 
         if title_match >= 0:
             header_lines += 1
             title_line = line
             if verbose:
-                print("Title on line " + str(header_lines) + ": " + str(title_line))
+                print(("Title on line " + str(header_lines) + ": " + str(title_line)))
             mode = 0
         elif var_match >= 0:  # match lines starting with variable keyword, add the following var names to list
             header_lines += 1
-            # split and get words enclosed by double quotations, return only the odd elements
+            # split and get words enclosed by double quotations, return only the odd elements (http://stackoverflow.com/questions/2076343/extract-string-from-between-quotations)
             l = line.split('"')[1::2]
             l = ['"{}"'.format(word) for word in l]
             if verbose:
                 print(l)
             variables = l
-
+            '''
+            problem with this approach: VARIABLES list is not always separated by commas
+            # remove all whitespace in the line, i.e. cast "VARIABLES = " to "VARIABLES=" to make matching the keyword easier
+            line = ''.join(line.split())
+            var1 = line.split("VARIABLES=", 1).pop()
+            for var in var1.split(','):
+                variables.append(var)
+            '''
+            #        varmode=1
+            # num_of_var = variables.count('"') / 2
             if verbose:
-                print("Variables on line " + str(header_lines) + ": " + str(variables))
+                print(("Variables on line " + str(header_lines) + ": " + str(variables)))
             mode = 1
 
         elif zone_match >= 0:
             if mode == 3: # that is, if we have been reading a previous zone
                 if struct == 1: #only if we had no nodes or other info, otherwise we'd add double the amount of DFs
                     if verbose:
-                        print 'found new zone'
+                        print('found new zone')
                     dfs.append(pd.DataFrame(data))
                 mode = 2
 #                continue
@@ -92,11 +110,16 @@ def read_tecplot(filename, verbose):
             # parse the actual line
             header_lines += 1
             zone = line
+            # idxi_match = line.find("I=")
+            # idxj_match = line.find("J=")
             zone = line.split("ZONE T=", 1).pop()
+            # split the string at the quotation marks, to ensure that only what is inside them gets used as zone name
+            zonesplit = zone.split('"')
+            zone = '"'.join(zonesplit[:2]) + '"'
             zones.append(zone)
             varmode = 0
             if verbose:
-                print("Zone on line " + str(header_lines) + ": " + str(zone))
+                print(("Zone on line " + str(header_lines) + ": " + str(zone)))
             mode = 2  # vars are apparently finished, shift mode to 2
             data = []
 
@@ -107,7 +130,7 @@ def read_tecplot(filename, verbose):
             b = ","
             nodes = int(line.split(a)[-1].split(b)[0])
             if verbose:
-                print ("found nodes= " + str(nodes))
+                print(("found nodes= " + str(nodes)))
             struct = 0 # if Nodes keyword is present, we read unstructured data
             header_lines += 1
             #print str(mode)
@@ -116,7 +139,7 @@ def read_tecplot(filename, verbose):
                 b = ","
                 elements = int(line.split(a)[-1].split(b)[0])
                 if verbose:
-                    print ("found elements= " + str(elements))
+                    print(("found elements= " + str(elements)))
 
 
         elif elems_match >= 0:
@@ -124,29 +147,29 @@ def read_tecplot(filename, verbose):
             b = ","
             elements = int(line.split(a)[-1].split(b)[0])
             if verbose:
-                print ("found elements= " + str(elements))
+                print(("found elements= " + str(elements)))
 
             header_lines += 1
 
         elif dt_match >= 0:
             header_lines += 1
             if verbose:
-                print "found DT, mode is " + str(mode)
+                print("found DT, mode is " + str(mode))
             data_match = 1
             mode = 3  # DT is for sure the last line of ZONE
         elif (line.find('STRANDID') >= 0) or (line.find('DATAPACKING') >= 0):
             header_lines += 1
             if verbose:
-                print "found STRANDID or DATAPACKING, mode remains at " + str(mode)
+                print("found STRANDID or DATAPACKING, mode remains at " + str(mode))
         # mode remains unchanged, reading data
         elif mode == 3:
             if struct == 1 or len(data) < nodes: # read data
                 data.append(line.split())
                 if struct == 0 and len(data) == nodes:
                     if verbose:
-                        print ("found last unstructured data line, overall length: " + str(len(data)))
-                        print (type(data))
-                        print len(els)
+                        print(("found last unstructured data line, overall length: " + str(len(data))))
+                        print((type(data)))
+                        print(len(els))
 
                     elems=[]
                     dfs.append(pd.DataFrame(data))
@@ -159,16 +182,30 @@ def read_tecplot(filename, verbose):
             else:
                 continue  # finished reading data, now doing elements
 
+#            if struct == 0 and len(data) == nodes:
+#                if verbose:
+#                    #print(data[-1])
+#                    #print(data[0])
+#                    print ("found last unstructured data line, overall length: " + str(len(data)))
+#                    print (type(data))
+#                elems=[]
+#                print ("lendatanodes")
+#                dfs.append(pd.DataFrame(data))
+#            elif struct == 0 and len(elems) < elements:
+#                elems.append(line.split())
+#            elif struct == 0 and len(elems) == elements:
+#                els.append(elems)
+
         elif (mode==2 and is_number(line.split()[0])): # this is true if we are below the ZONE keyword and at a data line
             mode = 3
             if verbose:
-                print 'found first data line'
+                print('found first data line')
             data.append(line.split())
 
 
         elif mode == 1:  # lines between VARIABLES and ZONE keywords: these may be additional variables or AUXDATA 
             if verbose:
-                print("More variables on line " + str(header_lines) + ": " + str(line))
+                print(("More variables on line " + str(header_lines) + ": " + str(line)))
             if line.startswith( 'DATASETAUX' ): # TODO: match other AUX data
                 continue
             else:
@@ -180,14 +217,19 @@ def read_tecplot(filename, verbose):
         if mode == 3:
             if struct == 1 or len(data) < nodes:
                 dfs.append(pd.DataFrame(data))
-
     zones = [item.strip() for item in zones]
-
+    if verbose:
+        print(zones)
+    #print(variables)
     
-
-    # set variables as column names of each pandas dataframe
+    # final step:
+    # - set variables as column names
+    # - cast the dataframe to numeric. we can do this because Tecplot zones are all numeric
     for df in dfs:
-	df.columns = variables
-        df.apply(pd.to_numeric)
+        #print variables
+        #print df.describe
+        df.columns = variables
 
+        #print df.columns
+        df.apply(pd.to_numeric)
     return zones, dfs, els
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Andreas Waldmann",
+    author_email='waldmann@iag.uni-stuttgart.de',
+    description="Tecplot binary reader",
+    license="MIT license",
+    include_package_data=True,
+    keywords='tecplot',
+    name='pyTecIO',
+    url='https://github.com/awaldm/pyTecIO',
+    version='0.1.0',
+    zip_safe=False,
+)
